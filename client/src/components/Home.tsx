003dd9c61de96dc2b6b645623b6429756d2531cf
Jos Codes <joselineruiz917@gmail.com>
2023-01-11 09:13:17 -0600

    use state & use effect

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,65 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import './Home.css'
 
-
 /* eslint-disable */
 
-interface Props {
-  value?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
-  name?: string;
+
+export const Home: React.FC = () => {
+
+   
+//Type definition for suspect
+interface ISuspect {
+  id: number;
+  name: string;
+  placeOfCrime: string;
+  NumberOfCrimes: number;
+  NumberOfVictims: number;
+  DateOfCrimes: string;
+  JailTime: string;
+  DateOfIncarceration: string;
+  disorders: string;
+  Facts: string;
 }
- 
-export const Home: React.FC<Props> = ({ value, onChange, placeholder, name }) => {
 
   //usestate
-  const UsingFetch = () => {
-    const [killers, setKillers] = useState([]) 
+    const [viewKillers, saveKillers] = useState([]) 
+    const [searchInput, storeSearchInput] = useState([])
 
     const fetchData = () =>  {
-      fetch("src/mockApi/truecrime.json")
+      fetch("/src/mockApi/truecrime.json")
         .then(response => {
           return response.json()
         })
-        .then(data => {
-          setKillers(data)
-          console.log(data);
+        .then(response => {
+          saveKillers(response)
+          console.log("fetch response", response);
         })
     }
-  }
 
   //useeffect ( fetch inside & set the response to the state to log to the jsx below ( to the screen))
   useEffect(() => {
     fetchData()
+    console.log("killerState", viewKillers)
   }, [])
+
+const handleSearch = (event) => {
+storeSearchInput(event.target.value)
+} 
+
 
   return (
     <div>
-      {data.length > 0 && (
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-  )
-}
-
-export default UsingFetch
-  
-
-  return (
-    
-    <React.Fragment>
-      <h1>True Crime</h1>
+       <h1>True Crime</h1>
       <div>
       <input className="input" type="text"
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        name={name}
+        value={searchInput}
+        onChange={handleSearch}
+        placeholder= "typeInKiller"
     />
        </div>
 
@@ -68,14 +63,15 @@ export default UsingFetch
       onClick={() => console.log('clicked')}>
         Search
      </button>
-    </React.Fragment>
-  );
-};
+        {/* <ul>
+          {viewKillers.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul> */}
+      
+    </div>
 
-
-
-
-function fetchData() {
-  throw new Error('Function not implemented.');
+  )
 }
+
 
